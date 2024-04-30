e69ba95088213e6b78c930f42708c21bd70d54db
Francisco González <fcoglez.politecnico@gmail.com>
2024-04-30 13:27:11 +0200

    configuration of the .env file and the configuration database file

---
+++ database/config.js
@@ -0,0 +1,23 @@
+//importacion del paquete
+const mongoose = require('mongoose');
+
+const dbConnection = async () => {
+
+    try {
+        await mongoose.connect(process.env.DB_CONNECTION , {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log('DB Online');
+        
+    } catch (error) {
+        console.log(error);
+        // throw new Error('Error to connect');
+    }
+
+}
+
+module.exports = {
+    dbConnection
+}
