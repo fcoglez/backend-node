0274a0a4eef93ec590f181c30b0a7196685f07b9
Francisco González <fcoglez.politecnico@gmail.com>
2024-04-30 13:29:29 +0200

    news package of npm

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 //ASI SE HACE UNA IMPORTACION EN NODE
+require('dotenv').config();
+
 const express = require('express');
+const cors = require('cors');
+
+const { dbConnection } = require('./database/config');
 
 //CREAR EL SERVIDOR DE EXPRESS
 const app = express();
+
+//CONFIGURAR CORS
+app.use(cors());
+
+//Llamada la base de datos
+dbConnection();
 
 
 //Routes
