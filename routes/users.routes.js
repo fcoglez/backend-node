7fcbd15529b34e9b95a2caca75dfaec3305f8a75
Francisco González <franciscogonzalezhontoria@gmail.com>
2024-05-06 13:51:06 +0200

    implementing route for update users

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,16 +2,38 @@
     Ruta: /api/users
 */ 
 const { Router } = require('express');
-const { getUsers, postUsers } = require('../controllers/users.controller');
+const { check } = require('express-validator');
+
+const { validateFields } = require('../middlewares/validate-fields.middleware');
+const { getUsers, postUsers, putUsers } = require('../controllers/users.controller');
 
 const router = Router();
 
-
+//GET
 router.get('/', getUsers );
 
 
+//POST
+router.post('/', 
+    [
+        check('name', 'the name is obligatory').not().isEmpty(),
+        check('email', 'the email is obligatory').isEmail(),
+        check('password', 'the password is obligatory').not().isEmpty(),
+        validateFields,
+    ],
+    postUsers
+);
 
-router.post('/', postUsers);
+
+//PUT
+router.put('/:id',
+    [
+        check('name', 'the name is obligatory').not().isEmpty(),
+        check('email', 'the email is obligatory').isEmail(),
+        // check('role', 'the role is obligatory').isEmail(),
+    ],
+    putUsers 
+);
 
 
 
